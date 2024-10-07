c76ad9ee405d596294aeca9da1472f0c09b93521
Jo <82283920+JoDotNet@users.noreply.github.com>
2024-10-07 17:32:32 +0200

    Refactor __init__.py to include YouTube class

--- src/JoDBS_Tools/__init__.py
+++ src/JoDBS_Tools/__init__.py
@@ -4,6 +4,7 @@ from .Database import Database, BotNetworkConnection
 from .Decorators import Permission_Checks
 from .utils import Get_ENV, Load_ENV
 from .BotSetup import BotSetup
+from .YouTube import YouTube
 
 __all__ = [
     'Database',
@@ -11,5 +12,6 @@ __all__ = [
     'Permission_Checks',
     'Get_ENV',
     'Load_ENV',
-    'BotSetup'
+    'BotSetup',
+    "YouTube"
 ]
