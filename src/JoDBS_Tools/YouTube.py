6c2bf4532d93c2a4e70baac294d5e342b47e3331
Jo <82283920+JoDotNet@users.noreply.github.com>
2024-10-07 16:20:09 +0200

    Refactor YouTube class to include method for retrieving the latest video from a channel

---
+++ src/JoDBS_Tools/YouTube.py
@@ -0,0 +1,32 @@
+import requests
+
+class YouTube:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = 'https://www.googleapis.com/youtube/v3'
+
+    def get_latest_video(self, channel_id):
+        url = f'{self.base_url}/search'
+        params = {
+            'part': 'snippet',
+            'channelId': channel_id,
+            'maxResults': 1,
+            'order': 'date',
+            'type': 'video',
+            'key': self.api_key
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        if 'items' in data and len(data['items']) > 0:
+            video = data['items'][0]
+            video_id = video['id']['videoId']
+            title = video['snippet']['title']
+            description = video['snippet']['description']
+            return {
+                'video_id': video_id,
+                'title': title,
+                'description': description
+            }
+        else:
+            return None
