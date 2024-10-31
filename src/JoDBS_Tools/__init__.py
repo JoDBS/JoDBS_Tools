e2032e72625362e294c13622b97fb835f98d9548
Jo <82283920+JoDotNet@users.noreply.github.com>
2024-10-31 18:05:49 +0100

    Rename Database import to MongoClientConnection for clarity

--- src/JoDBS_Tools/__init__.py
+++ src/JoDBS_Tools/__init__.py
@@ -1,13 +1,13 @@
 # src/JoDBS_Tools/__init__.py
 
-from .Database import Database, BotNetworkConnection
+from .Database import MongoClientConnection, BotNetworkConnection
 from .Decorators import Permission_Checks
 from .utils import Load_ENV, Get_ENV, Get_UnixTimestamp_UTC, Get_UnixTime_UTC, save_json, load_json
 from .BotSetup import BotSetup
 from .YouTube import YouTube
 
 __all__ = [
-    'Database',
+    'MongoClientConnection',
     'BotNetworkConnection',
     'Permission_Checks',
     'Load_ENV',
