46fec6c08e7eae29192c0fa5532826b7994827c4
Jo <82283920+JoDotNet@users.noreply.github.com>
2024-11-04 14:48:57 +0100

    Refactor get_highest_role_without_color function to remove unnecessary self parameter

--- src/JoDBS_Tools/utils.py
+++ src/JoDBS_Tools/utils.py
@@ -120,7 +120,7 @@ def load_json(file_path=None):
 
 # Methods related to the Discord API
 from nextcord import Member, Colour
-async def get_highest_role_without_color(self, member: Member):
+async def get_highest_role_without_color(member: Member):
         roles_without_colour = [role for role in member.roles if role.colour != Colour.default()]
         roles_without_colour.sort(key=lambda role: role.position, reverse=True)
         return roles_without_colour[0] if roles_without_colour else None
