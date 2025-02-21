b079d909127df3b2847bc6c63472621875e090db
Jo <82283920+JoDotNet@users.noreply.github.com>
2025-02-21 20:25:37 +0100

    Bump version to 0.2.5-dev75 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='JoDBS_Tools',
-    version='0.2.5-dev74',
+    version='0.2.5-dev75',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
