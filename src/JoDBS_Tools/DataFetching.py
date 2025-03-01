0b02cfd4ed265fee64130a9ffffb328667f55d3f
Jo <82283920+JoDotNet@users.noreply.github.com>
2025-03-01 16:11:11 +0100

    Enhance DataFetching: add debug mode, implement get_version method, and refactor data folder handling

--- src/JoDBS_Tools/DataFetching.py
+++ src/JoDBS_Tools/DataFetching.py
@@ -1,28 +1,49 @@
 import os
 from .Database import BotNetworkConnection
-from .utils import save_json
-
+from .utils import save_json, load_json
 
 class DataFetching:
-    def __init__(self):
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.data_folder = "./data"
         self.__create_data_folder()
         self.BNC = BotNetworkConnection()
 
     def __create_data_folder(self):
         try:
-            os.makedirs("data")
+            os.makedirs(self.data_folder)
             print("> DataFetching.py: Created data folder.")
             return
         except FileExistsError:
             return
 
-    def get_roles_json(self):
-        print(f"> DataFetching.py: Attempting to get roles.json from BotNetworkConnection.")
-        roles = self.BNC.fetch_and_save_roles()
-        if roles:
-            print(f"> DataFetching.py: Successfully fetched roles.json from BotNetworkConnection.")
-        else:
-            print(f"> DataFetching.py: Failed to fetch roles.json from BotNetworkConnection.")
+    # def get_roles_json(self):
+    #     print(f"> DataFetching.py: Attempting to get roles.json from BotNetworkConnection.")
+    #     roles = self.BNC.fetch_and_save_roles()
+    #     if roles:
+    #         print(f"> DataFetching.py: Successfully fetched roles.json from BotNetworkConnection.")
+    #     else:
+    #         print(f"> DataFetching.py: Failed to fetch roles.json from BotNetworkConnection.")
+
+    def get_version(self):
+        try:
+            scope = "version"
+            data = self.BNC.get_data(scope=scope)
+
+            if self.debug:
+                print(f"> {__file__}: Getting version from BotNetworkConnection")
+                print(f"> {__file__}: Data: {data}")
+            
+            if data:
+                print(f"> {__file__}: Successfully fetched version from BotNetworkConnection.")
+                save_json(data, f"{self.data_folder}/{scope}.json")
+                
+        except Exception as e:
+            print(f"> {__file__}: Failed to fetch version from BotNetworkConnection.")
+            print(f"> {__file__}: Error: {e}")
+
+
+
 
     def get_all_available(self):
 
