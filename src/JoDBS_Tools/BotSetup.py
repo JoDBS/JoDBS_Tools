78f10f09fa9acaeb070fe3db3abb004540ab3a1c
Jo <82283920+JoDotNet@users.noreply.github.com>
2025-03-01 21:40:38 +0100

    Refactor BotSetup to load version from JSON and simplify Bot status handling

--- src/JoDBS_Tools/BotSetup.py
+++ src/JoDBS_Tools/BotSetup.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from nextcord import Game
 from .Database import BotNetworkConnection
 from .DataFetching import DataFetching
-from .utils import Get_ENV, Load_ENV, Get_ENV_Bool
+from .utils import Get_ENV, Load_ENV, Get_ENV_Bool, load_json
 
 class BotSetup:
     def __init__(self, bot, debug=False, env_path=None, NodeConnection=True):
@@ -20,6 +20,7 @@ class BotSetup:
         self.token = Get_ENV(key="TOKEN")
         self.cogs_directory = "./cogs"
         self.BNC = BotNetworkConnection() if self.NodeConnection else None 
+        self.version = load_json("./data/version.json")["version"] or "N/A"
 
     def run_bot(self):
         try:
@@ -60,22 +61,17 @@ class BotSetup:
                     print("Bot Setup failed to run;\n BotNetworkConnection failed. Check ENV variables.")
                     return
                 print(status)
+                # print("BotNetworkConnection is enabled.") Put this in status message in bot-database
 
                 # Fetch data from BNC
-                data_fetching = DataFetching()
-                
+                data_fetching = DataFetching(debug=self.debug)
+                print("Fetching Data:")
+                data_fetching.get_all_available_scopes()      
 
-
-                # # Check if bot can get roles.json from BotNetwork
-                # print("Retrieving Roles from BotNetwork:")
-                # roles_fetch = self.BNC.fetch_and_save_roles()
-                # if roles_fetch:
-                #     print("Roles fetched successfully.")
-                # else:
-                #     print("Roles fetch failed. (some cogs might error out if they rely on roles.json)")
-                #     print("Update roles.json in BotNetwork.")
             else:
                 print("BotNetworkConnection is disabled.\n Some features might not work if cogs rely on BNC functions.")
+
+
             print("=====BOT=====")
             print("Loading Cogs:")
             self.add_cogs()
@@ -89,22 +85,14 @@ class BotSetup:
             print(f"Bot Setup failed to run; BotNetworkConnection failed, or cogs failed to run. Check ENV variables.")
             print(f"Error: {e}")
 
-    
     async def getBotStartupInfo(self):
         try:
-            # Initiate a BotNetworkConnection to get version.
-            if self.NodeConnection:
-                # BNC = BotNetworkConnection(base_url=Get_ENV("BNC_BASE_URL"), api_key=Get_ENV("BNC_API_KEY"))
-                version = self.BNC.get_data(scope="version")
-            else:
-                version = "N/A"
-
             launch_time = str(datetime.now())[0:19]
             user = self.bot.user
 
             end_time = datetime.timestamp(datetime.now())
             elapsed_time = round(end_time - self.start_time, 2)
-            launch_message = f"Launched with Version {version} at {launch_time}"+ "\n" + f"Logged in as: {user}" + "\n" +f"Elapsed time: {elapsed_time}s" + "\n" + "\n"
+            launch_message = f"Launched with Version {self.version} at {launch_time}"+ "\n" + f"Logged in as: {user}" + "\n" +f"Elapsed time: {elapsed_time}s" + "\n" + "\n"
 
             return launch_message
         except:
@@ -112,19 +100,11 @@ class BotSetup:
     
     async def setBotStatus(self):
         try:
-            if self.NodeConnection:
-                # Initiate a BotNetworkConnection to get version.
-                # BNC = BotNetworkConnection(base_url=Get_ENV("BNC_BASE_URL"), api_key=Get_ENV("BNC_API_KEY"))
-                version = self.BNC.get_data(scope="version")
-                if version is None:
-                    return False
-                activity_name = f"with v{version}"
-                activity = Game(name=activity_name)
-                await self.bot.change_presence(activity=activity)
-                return True
-            else:
-                print("BotNetworkConnection is disabled.\n Bot Status will not be set.")
-                return False
+            activity_name = f"with v{self.version}"
+            activity = Game(name=activity_name)
+            await self.bot.change_presence(activity=activity)
+            
+            return True
         except:
             print("Failed to set Bot Status.")
             return False
